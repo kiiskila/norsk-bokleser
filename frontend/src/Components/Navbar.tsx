91cc98f2e15c7e6cbf3a445e0282bce67bd382b7
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-22 17:40:13 -0600

    refactor navbar

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,125 +1,116 @@
-import React from "react";
-import { Link, Box, Flex, Text, Button, Stack } from "@chakra-ui/react";
-
+import React, { useState } from "react";
+import {
+  Link,
+  Box,
+  Flex,
+  Text,
+  Button,
+  Stack,
+  IconButton,
+} from "@chakra-ui/react";
 import { LogoWithText } from "./Logo";
 import { CloseIcon, HamburgerIcon } from "@chakra-ui/icons";
 
-const Navbar = (props: any) => {
-  const [isOpen, setIsOpen] = React.useState(false);
+type MenuItemProps = {
+  children: React.ReactNode;
+  to?: string;
+  isLast?: boolean;
+};
+
+type MenuToggleProps = {
+  toggle: () => void;
+  isOpen: boolean;
+};
+
+type NavBarContainerProps = {
+  children: React.ReactNode;
+};
+
+const Navbar: React.FC = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
 
   return (
     <NavBarContainer {...props}>
-      <Link href="/">
-        <LogoWithText />
-      </Link>
+      <LogoLink />
       <MenuToggle toggle={toggle} isOpen={isOpen} />
       <MenuLinks isOpen={isOpen} />
     </NavBarContainer>
   );
 };
 
-const MenuToggle = ({ toggle, isOpen }: any) => {
-  return (
-    <Box
-      display={{ base: "block", md: "none" }}
-      onClick={toggle}
-      role="button"
-      tabIndex={0}
-      onKeyDown={(e) => e.key === "Enter" && toggle()}
-      aria-label={isOpen ? "Close menu" : "Open menu"}
-    >
-      {isOpen ? (
-        <CloseIcon boxSize={4} cursor="pointer" />
-      ) : (
-        <HamburgerIcon boxSize={6} cursor="pointer" />
-      )}
-    </Box>
-  );
-};
+const LogoLink = () => (
+  <Link href="/">
+    <LogoWithText />
+  </Link>
+);
 
-const MenuItem = ({ children, isLast, to = "/", ...rest }: any) => {
-  return (
-    <Link href={to}>
-      <Text display="block" {...rest}>
-        {children}
-      </Text>
-    </Link>
-  );
-};
+const MenuToggle: React.FC<MenuToggleProps> = ({ toggle, isOpen }) => (
+  <IconButton
+    display={{ base: "block", md: "none" }}
+    icon={isOpen ? <CloseIcon boxSize={4} /> : <HamburgerIcon boxSize={6} />}
+    onClick={toggle}
+    aria-label={isOpen ? "Close menu" : "Open menu"}
+    variant="ghost"
+  />
+);
 
-const MenuLinks = ({ isOpen }: any) => {
-  return (
-    <Box
-      display={{ base: isOpen ? "block" : "none", md: "block" }}
-      flexBasis={{ base: "100%", md: "auto" }}
-    >
-      <Stack
-        spacing={8}
-        align="center"
-        justify={["center", "space-between", "flex-end", "flex-end"]}
-        direction={["column", "row", "row", "row"]}
-        pt={[4, 4, 0, 0]}
-      >
-        <MenuItem to="/">Home</MenuItem>
-        <MenuItem to="/">Report an Issue </MenuItem>
-        <MenuItem to="/">Settings </MenuItem>
-        <MenuItem to="/" isLast>
-          <Button
-            size="sm"
-            rounded="md"
-            color={[
-              "darkAccent.500",
-              "darkAccent.500",
-              "lightBackground",
-              "lightBackground",
-            ]}
-            bg={[
-              "lightBackground",
-              "lightBackground",
-              "darkAccent.500",
-              "darkAccent.500",
-            ]}
-            _hover={{
-              bg: [
-                "lightBackground",
-                "lightBackground",
-                "darkAccent.700",
-                "darkAccent.700",
-              ],
-            }}
-          >
-            Request a Book
-          </Button>
-        </MenuItem>
-      </Stack>
-    </Box>
-  );
-};
-
-const NavBarContainer = ({ children, ...props }: any) => {
-  return (
-    <Flex
-      as="nav"
-      align="center"
-      justify="space-between"
-      wrap="wrap"
-      w="100%"
-      mb={8}
-      p={8}
-      bg={["darkAccent.300", "darkAccent.300", "transparent", "transparent"]}
-      color={[
-        "lightBackground",
-        "lightBackground",
-        "darkAccent.500",
-        "darkAccent.500",
-      ]}
-      {...props}
-    >
+const MenuItem: React.FC<MenuItemProps> = ({ children, to = "/", ...rest }) => (
+  <Link href={to}>
+    <Text display="block" {...rest}>
       {children}
-    </Flex>
-  );
-};
+    </Text>
+  </Link>
+);
+
+const MenuLinks: React.FC<{ isOpen: boolean }> = ({ isOpen }) => (
+  <Box
+    display={{ base: isOpen ? "block" : "none", md: "block" }}
+    flexBasis={{ base: "100%", md: "auto" }}
+  >
+    <Stack
+      spacing={8}
+      align="center"
+      justify={["center", "space-between", "flex-end", "flex-end"]}
+      direction={["column", "row", "row", "row"]}
+      pt={[4, 4, 0, 0]}
+    >
+      <MenuItem to="/">Home</MenuItem>
+      <MenuItem to="/">Report an Issue</MenuItem>
+      <MenuItem to="/">Settings</MenuItem>
+      <MenuItem to="/" isLast>
+        <Button size="sm" rounded="md" colorScheme="teal">
+          Request a Book
+        </Button>
+      </MenuItem>
+    </Stack>
+  </Box>
+);
+
+const NavBarContainer: React.FC<NavBarContainerProps> = ({
+  children,
+  ...props
+}) => (
+  <Flex
+    as="nav"
+    align="center"
+    justify="space-between"
+    wrap="wrap"
+    w="100%"
+    mb={8}
+    p={8}
+    bg={["darkAccent.300", "darkAccent.300", "transparent", "transparent"]}
+    color={[
+      "lightBackground",
+      "lightBackground",
+      "darkAccent.500",
+      "darkAccent.500",
+    ]}
+    {...props}
+  >
+    {children}
+  </Flex>
+);
 
 export default Navbar;
