8196e3b297d97813f25fc2b4dee4f1ac0a917846
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-22 22:31:21 -0600

    refactor reader

--- frontend/src/Components/Reader/Reader.tsx
+++ frontend/src/Components/Reader/Reader.tsx
@@ -13,12 +13,30 @@ import {
 } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
 
+const highlightedStyle = {
+  backgroundColor: "teal",
+  color: "white",
+  borderRadius: "4px",
+  boxShadow: "0 0 0 4px teal",
+  display: "inline",
+  margin: "0 3px",
+  lineHeight: "inherit",
+};
+
+const normalStyle = {
+  backgroundColor: "transparent",
+  color: "inherit",
+  lineHeight: "inherit",
+  display: "inline",
+  margin: "0 3px",
+};
+
 function Reader() {
   const [book, setBook] = useState<book>();
   const [chapters, setChapters] = useState<chapter[]>([]);
   const [chosenChapter, setChosenChapter] = useState<string>();
   const [isTranslateOn, setIsTranslateOn] = useState(false);
-  const [bodyArray, setBodyArray] = useState([]);
+  const [bodyArray, setBodyArray] = useState<string[]>([]);
   const [preTranslatedText, setPreTranslatedText] = useState("Hello World");
   const [postTranslatedText, setpostTranslatedText] = useState("Hei Verden");
   const [selectedWordIndex, setSelectedWordIndex] = useState<number | null>(
@@ -27,85 +45,87 @@ function Reader() {
   const params = useParams();
   const toast = useToast();
 
-  const highlightedStyle = {
-    backgroundColor: "teal",
-    color: "white",
-    borderRadius: "4px",
-    boxShadow: "0 0 0 4px teal",
-    display: "inline",
-    margin: "0 3px",
-    lineHeight: "inherit",
-  };
-
-  const normalStyle = {
-    backgroundColor: "transparent",
-    color: "inherit",
-    lineHeight: "inherit",
-    display: "inline",
-    margin: "0 3px",
-  };
-
   const fetchData = useCallback(async () => {
-    const response = await fetch(`/read/${params.bookSlug}`);
-    const resMessage =
-      response.status === 404 ? "Book not found" : "Internal server error";
-
-    if (!response.ok) {
-      toast({
-        title: "Error",
-        description: resMessage,
-        status: "error",
-        duration: 2000,
-        isClosable: true,
-      });
-    } else {
+    try {
+      const response = await fetch(`/read/${params.bookSlug}`);
+      if (!response.ok) {
+        throw new Error(
+          response.status === 404 ? "Book not found" : "Internal server error"
+        );
+      }
       const data = await response.json();
       setBook(data.book);
       setChapters(data.chapters);
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast({
+          title: "Error",
+          description: error.message,
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: "An unexpected error occurred",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      }
     }
   }, [params.bookSlug, toast]);
 
-  function changeChapter(event: React.FormEvent<HTMLSelectElement>) {
-    const chapterId: string = event.currentTarget.value;
-
-    setChosenChapter(chapterId);
-  }
-
   const fetchChapter = useCallback(async () => {
-    if (isNaN(+chosenChapter!)) {
-      return;
-    }
-    const response = await fetch(`/read/${params.bookSlug}/${chosenChapter}`);
+    const chapterNumber = Number(chosenChapter);
+    if (isNaN(chapterNumber) || chapterNumber <= 0) return;
 
-    if (!response.ok) {
-      const message = `An error has occured: ${response.status}`;
-      throw new Error(message);
+    try {
+      const response = await fetch(`/read/${params.bookSlug}/${chosenChapter}`);
+      if (!response.ok) {
+        throw new Error(`An error has occurred: ${response.status}`);
+      }
+      const chapter = await response.json();
+      setBodyArray(chapter.body.split(/ /));
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        toast({
+          title: "Error",
+          description: error.message,
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      } else {
+        toast({
+          title: "Error",
+          description: "An unexpected error occurred",
+          status: "error",
+          duration: 2000,
+          isClosable: true,
+        });
+      }
     }
+  }, [chosenChapter, params.bookSlug, toast]);
 
-    const chapter = await response.json();
-    const extractWords = chapter.body.split(/ /);
-
-    setBodyArray(extractWords);
-  }, [chosenChapter, params.bookSlug]);
-
-  const handleClick = (word: string, index: number) => {
-    if (!isTranslateOn) {
-      return;
-    }
-    if (selectedWordIndex === index) {
-      setSelectedWordIndex(null);
-      setPreTranslatedText("");
-      translateText("");
-    } else {
-      setSelectedWordIndex(index);
-      setPreTranslatedText(word);
-      translateText(word);
-    }
+  const changeChapter = (event: React.FormEvent<HTMLSelectElement>) => {
+    setChosenChapter(event.currentTarget.value);
   };
 
-  function translateText(textToTranslate: string) {
+  const handleClick = (word: string, index: number) => {
+    if (!isTranslateOn || selectedWordIndex === index) {
+      setSelectedWordIndex(null);
+      return;
+    }
+    setSelectedWordIndex(index);
+    setPreTranslatedText(word);
+    translateText(word);
+  };
+
+  const translateText = (textToTranslate: string) => {
     setpostTranslatedText(textToTranslate.split("").reverse().join(""));
-  }
+  };
 
   useEffect(() => {
     fetchChapter();
@@ -121,16 +141,14 @@ function Reader() {
       <Select
         placeholder="Select a chapter"
         width={["80%", "70%", "50%"]}
-        onChange={(e) => changeChapter(e)}
+        onChange={changeChapter}
         value={chosenChapter}
       >
-        {chapters.map(function (chapter, index) {
-          return (
-            <option key={index} value={chapter.number}>
-              {chapter.title || "Chapter: " + chapter.number}
-            </option>
-          );
-        })}
+        {chapters.map((chapter, index) => (
+          <option key={index} value={chapter.number}>
+            {chapter.title || `Chapter: ${chapter.number}`}
+          </option>
+        ))}
       </Select>
       <Button onClick={() => setIsTranslateOn(!isTranslateOn)}>
         Translate Mode
@@ -143,28 +161,19 @@ function Reader() {
               color={"darkText"}
               userSelect={isTranslateOn ? "none" : "auto"}
             >
-              {bodyArray.map((word: string, index: number) => {
-                const isWordSelected = index === selectedWordIndex;
-                const wordStyle =
-                  isWordSelected && isTranslateOn
-                    ? highlightedStyle
-                    : normalStyle;
-
-                return (
-                  <span
-                    onClick={() => handleClick(word, index)}
-                    key={index}
-                    id={`${index}`}
-                    style={
-                      isTranslateOn
-                        ? { ...{ cursor: "pointer" }, ...wordStyle }
-                        : wordStyle
-                    }
-                  >
-                    {word}{" "}
-                  </span>
-                );
-              })}
+              {bodyArray.map((word, index) => (
+                <span
+                  onClick={() => handleClick(word, index)}
+                  key={index}
+                  style={getStyleForWord(
+                    index,
+                    isTranslateOn,
+                    selectedWordIndex
+                  )}
+                >
+                  {word}{" "}
+                </span>
+              ))}
             </Text>
           </CardBody>
         </Card>
@@ -178,5 +187,19 @@ function Reader() {
     </VStack>
   );
 }
+
+const getStyleForWord = (
+  index: number,
+  isTranslateOn: boolean,
+  selectedWordIndex: number | null
+) => {
+  const isWordSelected = index === selectedWordIndex;
+  return isTranslateOn
+    ? {
+        cursor: "pointer",
+        ...(isWordSelected ? highlightedStyle : normalStyle),
+      }
+    : normalStyle;
+};
 
 export default Reader;
