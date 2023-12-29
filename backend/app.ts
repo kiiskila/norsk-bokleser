e997285f8a831fbadecf30cd21c99290979cc119
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-29 15:33:11 -0600

    fix backend dev and prod environments

--- backend/app.ts
+++ backend/app.ts
@@ -1,30 +1,34 @@
-import express, { Express, Request } from "express";
+import express, { Express, Request, Response } from "express";
 import cors, { CorsOptionsDelegate, CorsRequest } from "cors";
 import dotenv from "dotenv";
 import https from "https";
 import fs from "fs";
 import path from "path";
+import http from "http";
 
 dotenv.config();
 
-const privateKeyPath = path.join(__dirname, "../../certs/privkey.pem");
-const certificatePath = path.join(__dirname, "../../certs/cert.pem");
-const caPath = path.join(__dirname, "../../certs/chain.pem");
-
-const privateKey = fs.readFileSync(privateKeyPath, "utf8");
-const certificate = fs.readFileSync(certificatePath, "utf8");
-const ca = fs.readFileSync(caPath, "utf8");
-
-const credentials = { key: privateKey, cert: certificate, ca: ca };
-
-const api = require("./routes/api");
 const app: Express = express();
+const isProduction = process.env.NODE_ENV === "production";
+const port = isProduction ? 443 : 8080; // Use 443 for production and 8080 for local development
 
-const allowedOrigins = [
-  "http://localhost:3000",
-  "https://norsk-bokleser.vercel.app",
-  "http://norsk-bokleser.vercel.app",
-];
+// HTTPS setup for production
+let httpsServer;
+if (isProduction) {
+  const privateKeyPath = path.join(__dirname, "../../certs/privkey.pem");
+  const certificatePath = path.join(__dirname, "../../certs/cert.pem");
+  const caPath = path.join(__dirname, "../../certs/chain.pem");
+
+  const privateKey = fs.readFileSync(privateKeyPath, "utf8");
+  const certificate = fs.readFileSync(certificatePath, "utf8");
+  const ca = fs.readFileSync(caPath, "utf8");
+
+  const credentials = { key: privateKey, cert: certificate, ca: ca };
+  httpsServer = https.createServer(credentials, app);
+}
+
+// CORS setup
+const allowedOrigins = ["https://norsk-bokleser.vercel.app"];
 
 const corsOptionsDelegate: CorsOptionsDelegate<CorsRequest> = (
   req,
@@ -44,21 +48,30 @@ const corsOptionsDelegate: CorsOptionsDelegate<CorsRequest> = (
 
 app.use(cors(corsOptionsDelegate));
 app.use(express.json());
+
+// API routes
+const api = require("./routes/api");
 app.use("/", api);
 
-const httpsServer = https.createServer(credentials, app);
+// Start the server
+if (isProduction) {
+  httpsServer!.listen(443, () => {
+    console.log("HTTPS Server running on port 443");
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+  });
+}
 
-httpsServer.listen(443, () => {
-  console.log("HTTPS Server running on port 443");
-});
+// HTTP to HTTPS redirect (for production)
+if (isProduction) {
+  const httpApp = express();
+  httpApp.get("*", (req: Request, res: Response) => {
+    res.redirect("https://" + req.headers.host + req.url);
+  });
 
-const http = require("http");
-const httpApp = express();
-
-httpApp.get("*", (req, res) => {
-  res.redirect("https://" + req.headers.host + req.url);
-});
-
-http.createServer(httpApp).listen(80, () => {
-  console.log("HTTP Server running on port 80");
-});
+  http.createServer(httpApp).listen(80, () => {
+    console.log("HTTP Server running on port 80");
+  });
+}
