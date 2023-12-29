7d040c1b05af0229e5b3e5cf103ed2a5cb2d37f3
Will Kiiskila <kiiskila.will@gmail.com>
2023-12-28 23:10:05 -0600

    add cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,23 @@
 import express, { Express, Request, Response } from "express";
-import { PrismaClient } from "@prisma/client";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const api = require("./routes/api");
 const app: Express = express();
 const port = 8080;
-import dotenv from "dotenv";
 
-dotenv.config();
+const corsOptions = {
+  origin:
+    process.env.CORS_ORIGIN ||
+    "http://localhost:3000" ||
+    "https://norsk-bokleser.vercel.app" ||
+    "http://norsk-bokleser.vercel.app",
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
