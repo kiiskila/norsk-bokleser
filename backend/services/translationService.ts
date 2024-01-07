a4b9590cca02c0da5925670a9924975402fb5a9a
Will Kiiskila <kiiskila.will@gmail.com>
2024-01-06 22:31:13 -0600

    refactor translation service

--- backend/services/translationService.ts
+++ backend/services/translationService.ts
@@ -1,31 +1,20 @@
 import axios from "axios";
-const translate = require("translate-google");
+const translateGoogle = require("translate-google");
 
 const apiKey = process.env.DEEPL_KEY;
 const sourceLanguage = "NB"; // Norwegian (Bokmål)
 const targetLanguage = "EN-US"; // English (American)
 
-// Cache object
+// Cache object for both services
 const translationCache: { [key: string]: string } = {};
 
 export async function useDeepLTranslate(text: string): Promise<string> {
-  // Generate a unique cache key based on the text and target language
-  const cacheKey = `${text}-${targetLanguage}`;
-
-  // Check if the translation is already in the cache
+  const cacheKey = `DeepL-${text}-${targetLanguage}`;
   if (translationCache[cacheKey]) {
     return translationCache[cacheKey];
   }
 
   const url = "https://api-free.deepl.com/v2/translate";
-
-  try {
-    translate(text, { from: "no", to: "en" }).then((res: any) => {
-      console.log(res);
-    });
-  } catch (error) {
-    console.log(error);
-  }
 
   try {
     const response = await axios.post(url, null, {
@@ -37,25 +26,31 @@ export async function useDeepLTranslate(text: string): Promise<string> {
       },
     });
 
-    // Store the translation in the cache
     const translatedText = response.data.translations[0].text;
     translationCache[cacheKey] = translatedText;
 
     return translatedText;
   } catch (error) {
     console.error("Error calling DeepL API:", error);
-    throw new Error("Translation service failed");
+    throw new Error("DeepL Translation service failed");
   }
 }
 
 export async function useGoogleTranslate(text: string): Promise<string> {
-  let translatedText = "";
-  try {
-    await translate(text, { from: "no", to: "en" }).then((res: any) => {
-      translatedText = res;
-    });
-  } catch (error) {
-    console.log(error);
+  const cacheKey = `Google-${text}-en`;
+  if (translationCache[cacheKey]) {
+    return translationCache[cacheKey];
   }
-  return translatedText;
+
+  try {
+    const translatedText = await translateGoogle(text, {
+      from: "no",
+      to: "en",
+    });
+    translationCache[cacheKey] = translatedText;
+    return translatedText;
+  } catch (error) {
+    console.error("Error using Google Translate:", error);
+    throw new Error("Google Translate service failed");
+  }
 }
